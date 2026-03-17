ac667e29556c6239ee40a8d6c5dd48aac5e9aa6f
Rahul singh <rahuls6408@gmail.com>
2026-03-17 18:58:34 +0530

    added auth route and test api

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
 import express from "express"
 import authRoutes from "./routes/auth.routes"
+import { protect } from "./middleware/auth.middleware";
 import cors from "cors"
 const app=express()
 app.use(cors())
 app.use(express.json())
 app.use("/api/auth",authRoutes)
+app.get("/api/test", protect, (req, res) => {
+  res.json({ message: "Protected route working" });
+});
 app.get('/',(req,res)=>{
     res.send("API is running...")
 })
--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import { registerUser } from "../controllers/auth.controller";
+import { loginUser } from "../controllers/auth.controller";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
+router.post("/login", loginUser);
 
 export default router;
